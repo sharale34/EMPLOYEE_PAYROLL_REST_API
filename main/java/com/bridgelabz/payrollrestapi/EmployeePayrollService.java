c5eef7fc7745107584a68954412e0fa55c8b3640
sharale34 <sourabhharale8000@gmail.com>
2020-11-06 09:02:28 +0530

    Added: Welcome message is added

---
+++ main/java/com/bridgelabz/payrollrestapi/EmployeePayrollService.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.payrollrestapi;
+
+import java.util.logging.Logger;
+
+public class EmployeePayrollService {
+	public static void main(String[] args) {
+		Logger log = Logger.getLogger(EmployeePayrollService.class.getName());
+		log.info("Welcome to the Employee Payroll Rest API Program.");
+	}
+}
